2388e4768156e5263439b9edea6973bfb0ecc935
Exonerative <starscreamkn@gmail.com>
2025-10-19 23:43:28 +1100

    Add launch-all batch to start server and open browser

--- launch-all.bat
+++ launch-all.bat
@@ -4,13 +4,22 @@ setlocal enabledelayedexpansion
 :: Move to the directory containing this script
 cd /d "%~dp0"
 
-if not exist node_modules ( 
+if not exist node_modules (
     echo [TimeAuction] Installing dependencies...
     call npm install || goto :error
 )
 
-echo [TimeAuction] Starting server...
-call npm start
+echo [TimeAuction] Starting server in new window...
+start "TimeAuction Server" cmd /c "npm start"
+
+:: Allow the server a moment to boot before opening the browser
+for /l %%i in (1,1,5) do (
+    timeout /t 1 /nobreak >nul
+    echo [TimeAuction] Waiting for server to initialize... (%%i/5)
+)
+
+echo [TimeAuction] Launching game in browser...
+start "" http://localhost:3000/
 
 goto :eof
 
@@ -18,4 +27,3 @@ goto :eof
 echo.
 echo Failed to set up TimeAuction.
 pause
-
